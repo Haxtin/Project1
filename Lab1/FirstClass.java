6cf0e0950acf5447f5a5e3db541fb88d6bd226d4
Haxtin <dylanveghead@gmail.com>
2016-08-31 08:44:51 -0700

    Add files via upload

---
+++ Lab1/FirstClass.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class FirstClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int b;
+    private int a;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+       a = 2;
+       b = 5;
+        
+        // initialise instance variables
+      
+    }
+    public int addEmUp(){
+        return a + b;
+    }
+    public int difference(){
+        return a - b;
+    }
+        
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+   
+   
+}
---
+++ Lab1/FirstClassRunner.java
@@ -0,0 +1,14 @@
+
+/**
+ * /Dylan Fuhs 
+ * Lab 1
+ */
+public class FirstClassRunner
+{
+    public static void main(String[] args)
+    {
+       
+    }
+    
+    
+}
